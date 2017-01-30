a5a65b7a77591d5f6ee997a53babc7b612473909
Benjamin Marquardt <admin@freneticetchings.com>
2017-01-30 08:23:57 -0500

    Map initialization and testing size, etc.

--- systemstest.cpp
+++ systemstest.cpp
@@ -1,13 +1,27 @@
 /*
 * @Author: Benjamin Marquardt
 * @Date:   2017-01-26 13:19:23
-* @Last Modified by:   Benjamin
-* @Last Modified time: 2017-01-29 13:25:49
+* @Last Modified by:   Benjamin Marquardt
+* @Last Modified time: 2017-01-30 08:20:20
 */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "systemstest.h"
 
+
 int main(){
+ 	MAP_BUFFER *_mP = (MAP_BUFFER *)malloc(sizeof(MAP_BUFFER));
+ 	int iterator = MAP_SIDE_SIZE * MAP_SIDE_SIZE * MAP_HEIGHT_SIZE;
+ 	struct POS *p = &_mP->GRID[0];
+
+ 	for(int x = 0; x < iterator; x++)
+ 	{
+ 		p->X = 1.0f;
+ 		p->Y = 1.0f;
+ 		p++;
+ 	}
+ 	printf("Size of map: %f\n", (double) sizeof(*_mP));
+	system("PAUSE");
 	return 0;
 }
--- systemstest.h
+++ systemstest.h
@@ -4,19 +4,24 @@
 * @Last Modified by:   Benjamin Marquardt
 * @Last Modified time: 2017-01-29 14:01
 */
+#define CHUNK_SIZE 64
+#define MAP_SIDE_SIZE 64
+#define MAP_HEIGHT_SIZE 16
 
-
-typedef CHUNK_SIZE 		64;
-typedef MAP_SIDE_SIZE 	64;
-typedef MAP_HEIGHT SIZE 16;
-
-struct POS
+typedef struct POS
 {
-	float X  ;
+	float X;
 	float Y;
-};
+} POS;
 
 typedef struct MAP_BUFFER
 {
-	struct POS GRID[CHUNK_SIZE * MAP_SIZE * MAP_SIZE * MAP_HEIGHT_SIZE];
+	struct POS GRID[CHUNK_SIZE * MAP_SIDE_SIZE * MAP_SIDE_SIZE * MAP_HEIGHT_SIZE];
 } MAP_BUFFER;
+
+typedef struct MAP_PROPERTIES
+{
+	int X_OFFSET;
+	int Y_OFFSET;
+} MAP_PROPERTIES;
+
