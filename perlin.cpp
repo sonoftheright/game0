f21afa287802fc8c0b32b57e8a4db1097c4a3253
Benjamin Marquardt <son.of.the.right@gmail.com>
2017-02-14 13:24:39 -0500

    Various cleanup details; added Perlin's fade function.

--- perlin.cpp
+++ perlin.cpp
@@ -16,14 +16,20 @@ void shuffleIntArray(int length, int *array)
 	printf("First argument is %d, Second argument is %d\n", array[0], array[1]);
 }
 
+// Fade function - credit Ken Perlin and Flafla2 at 
+// http://flafla2.github.io/2014/08/09/perlinnoise.html              
+double fade(double t) 
+{
+    return t * t * t * (t * (t * 6 - 15) + 10);         
+}
+
 void initializePermutationArray(int seedArgument)
 {
-	if(permutationArray[0] == 0 && permutationArray[1] == 0)
-	{
-		for(int x = 0; x <= 255; x++) {	permutationArray[x]	= x; }
-	}
+	for(int x = 1; x <= 256; x++) {	permutationArray[x-1] = x; }
 	srand(seedArgument);
 	shuffleIntArray(255, permutationArray);
+	int p[512];
+	for(int x = 0; x < 512; x++) {	p[x] = permutationArray[x%256]; }
 }
 
 // float[] perlin(float x, float y, float z)
